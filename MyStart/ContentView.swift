4dce06e529dc3d2100f587cd58472c62c189d075
Legenda759 <oripov1994@gmail.com>
2022-01-17 16:52:15 +0500

    Initial Commit

---
+++ MyStart/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyStart
+//
+//  Created by Legenda_759 on 17/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
